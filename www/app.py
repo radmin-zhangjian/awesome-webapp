91b724d68ff3a9672de12e932596bbee12613ce1
radmin <6204000@qq.comsssss>
2018-03-04 16:01:31 +0800

    orm+models

--- www/app.py
+++ www/app.py
@@ -3,7 +3,7 @@
 
 import logging; logging.basicConfig(level=logging.INFO)
 
-import asyncio, os, json, time
+import orm, asyncio, os, json, time
 from datetime import datetime
 
 from aiohttp import web
@@ -11,15 +11,41 @@ from aiohttp import web
 def index(request) :
     return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
     
+async def on_close(app):
+    await orm.close_pool()
+    
 # @asyncio.coroutine    # 3.5 以前
 async def init(loop) :
     app = web.Application(loop=loop)
+    app.on_shutdown.append(on_close)
     app.router.add_route('GET', '/', index)
     # srv = yield from loop.create_server(app.make_handler(), '', 80)   # 3.5 以前
-    srv = await loop.create_server(app.make_handler(), '', 9000)
+    # srv = await loop.create_server(app.make_handler(), '', 9000)
+    handler = app.make_handler()
+    srv = await loop.create_server(handler, '127.0.0.1', 9000)
     logging.info('server started...')
-    return srv
+    # return srv
     
+    rs = dict()
+    rs['app'] = app
+    rs['srv'] = srv
+    rs['handler'] = handler
+    return rs
+    
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(init(loop))
+# loop.run_forever()
+
 loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+rs = loop.run_until_complete(init(loop))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    rs['srv'].close()
+    loop.run_until_complete(rs['srv'].wait_closed())
+    loop.run_until_complete(rs['app'].shutdown())
+    loop.run_until_complete(rs['handler'].finish_connections(60.0))
+    loop.run_until_complete(rs['app'].cleanup())
+loop.close()
---
+++ www/users.py
@@ -0,0 +1,13 @@
+import orm,asyncio
+from models import User,Blog,Comment
+
+# @asyncio.coroutine
+async def test(loop):
+    # yield from orm.create_pool(host='127.0.0.1',port=3306,user='root', password='', db='awesome',loop=loop)
+    await orm.create_pool(user='www-data', password='www-data', db='awesome',loop=loop)
+    u=User(name='test',email='test8@test.com',passwd='test',image='about:blank')
+    await u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
